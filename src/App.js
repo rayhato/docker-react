a4ba8bc09c14e75150f28b8070a775f446f47e28
rayhato <rayhato@gmail.com>
2019-05-12 18:42:17 +0300

    Testing branching feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Built inside of <code>docker-compose</code> with directory mapping
+          Pulled from <code>feature-branch</code>
         </p>
         <a
           className="App-link"
